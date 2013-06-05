190b97dcf5cfa978ef8ded3dcea89624dea1f61b
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-06-05 12:44:53 +0530

    new theam

--- index.php
+++ index.php
@@ -1,92 +1,54 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Anmol Water Tank Cleners</title>
-<link rel="stylesheet" href="styles.css" type="text/css" />
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/slider.js"></script>
-<script type="text/javascript" src="js/superfish.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
+<?php
 
+error_reporting(0);
+	include("include/database.php");
+	
+	if(isset($_REQUEST['login']))
+	{
+		$sql="select * from user where u_name = '". $_POST['username'] ."' and u_pass = '".$_POST['password']."'";
+		$result = mysql_query($sql);
+		if($row = mysql_fetch_array($result))
+		  {
+			 header("Location:home.php");
+		  }
+		else
+		{
+			header("location:index.php");
+		}
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Chaturang</title>
+
+<link href="login-box.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-<div id="container">
-	
-    <div id="nav">
-    	<ul class="sf-menu dropdown">
-        	
-        	<li class="selected"><a href="index.php">Home</a></li>
-            <li ><a class="has_submenu" href="site.php">Sites</a>
-            <ul>
-                	<li><a href="siteassgn.php">Assign To</a></li>
-                </ul>
-            </li>
-            <li ><a class="has_submenu" href="amcreport.php">AMC</a>
-            	<ul>
-                    <li><a href="amcreport.php">Amc Details</a></li>
-                </ul>
 
-            </li>
-            
-            
-            <li><a class="has_submenu" href="clients.php">Clients</a>
-            	<ul>
-                	<li><a href="addclients.php">Add Clients</a></li>
-                </ul>
-            
-            </li>
-            <li><a class="has_submenu" href="employee.php">Employees</a>
-            		<ul>
-                	<li><a href="addepm.php">Add Employee</a></li>
-                    <li><a href="employee.php">Employee Details</a></li>
-                    </ul>
-            
-            </li>
-            <li><a class="has_submenu" href="invoicedetails.php">Invoice Details</a>
-            		<ul>
-                	<li><a href="invoice.php">Invoice</a></li>
-                    </ul>
-            </li>
-            <li><a class="has_submenu" href="quotation.php">Quotation</a>
-            		<ul>
-                	<li><a href="quotationI.php">Quotation I</a></li>
-                    <li><a href="quotationII.php">Quotation II</a></li>
-                    </ul>
-            </li>
-            <li><a class="has_submenu"  href="term.php">Terms & Conditions</a>
-            <ul>
-              	<li><a href="addterm.php">Add Terms</a></li>
-            </ul>
-           </li>
-       
-        </ul>
-    </div>
-    
-    <div id="sub-header">
-		<div class="quo">
-    	<br />
-		<div class="quotation"><center>Todays Reminders</center></div>
-        <table class="emp_tab">
-        <tr class="emp_header">
-        <td width="150">Date</td>
-        <td width="250">Employee Name</td>
-        <td width="250">Client Name</td>
-        <td>Client Address</td>
-        <td width="150">Actual Date</td>
-        </tr>
-        </table>
-        <table class="emp_tab">
-    </div>
-    </div>
-        
-    
-    	<div class="clear"></div>
-    </div>
+
+<div style="padding: 130px 0 0 0;" align="center">
+
+<form action="" method="post">
+<div id="login-box">
+
+<H2 align="left"><span class="main">Anmol Water Tank</span><br /><span class="tour">Cleaners</span></H2>
+
+<div class="all">
+<div id="login-box-name" style="margin-top:20px;">User:</div><div id="login-box-field" style="margin-top:20px;"><input name="username" class="form-login" title="Username" placeholder="Enter User Name" size="30" maxlength="2048" /></div>
+<div id="login-box-name">Password:</div><div id="login-box-field"><input name="password" type="password" class="form-login" title="Password" placeholder="Enter Password" size="30" maxlength="2048" /></div>
+<br />
+<span class="login-box-options"><input type="checkbox" name="1" value="1"> Remember Me <a href="#" style="margin-left:30px;">Forgot password?</a></span>
+<br />
+<br />
+<div><input  class="log" type="submit" value="Login" name="login" /></div>
+</form>
 </div>
- <div id="footer">
-     <div class="clear"></div> 
-    </div>
-    </div>
+
+</div>
+
+</div>
 </body>
 </html>
